6344872122fd16c74a8c2ec41a21e2fe29a2e99b
KimHwon <hwon233@gmail.com>
2022-11-15 04:19:47 +0900

    Add Quantized MLP.

--- timm/models/layers/mlp.py
+++ timm/models/layers/mlp.py
@@ -4,6 +4,8 @@ Hacked together by / Copyright 2020 Ross Wightman
 """
 from torch import nn as nn
 
+from timm.models.quantization import quan_Linear, quan_Conv2d
+
 from .helpers import to_2tuple
 
 
@@ -123,4 +125,29 @@ class ConvMlp(nn.Module):
         x = self.act(x)
         x = self.drop(x)
         x = self.fc2(x)
+        return x
+
+
+class QuantizedMlp(nn.Module):
+    """ MLP as used in Vision Transformer, MLP-Mixer and related networks
+    """
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, bias=True, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        bias = to_2tuple(bias)
+        drop_probs = to_2tuple(drop)
+
+        self.fc1 = quan_Linear(in_features, hidden_features, bias=bias[0])
+        self.act = act_layer()
+        self.drop1 = nn.Dropout(drop_probs[0])
+        self.fc2 = quan_Linear(hidden_features, out_features, bias=bias[1])
+        self.drop2 = nn.Dropout(drop_probs[1])
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop1(x)
+        x = self.fc2(x)
+        x = self.drop2(x)
         return x
